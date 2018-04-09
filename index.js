87276cb815244e130030ce0ec63ad5f77f980004
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-04-09 21:48:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 
-function findMatching (drivers, name) {
+function findMatching(drivers, name) {
  return drivers.filter(function (driverName) {
  return driverName.toLowerCase() === name.toLowerCase()
     });
