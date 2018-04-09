30bda8a7e348625265897f954effe576d58b6b6c
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-04-09 21:39:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Code your solution in this file
-const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette',
-'Sarah', 'Bobby']
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette','Sarah', 'Bobby']
 
 function findMatching (drivers, name) {
  return drivers.filter(function (driver) {
