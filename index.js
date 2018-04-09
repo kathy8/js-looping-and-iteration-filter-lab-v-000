5ef20c72a150ba4c875d40ed092c233a5fba8857
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-04-09 21:45:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // Code your solution in this file
 
 function findMatching (drivers, name) {
- return drivers.filter(function (driver) {
- return driver.toLowerCase() === name.toLowerCase()
-    })
+ return drivers.filter(function (driverName) {
+ return driverName.toLowerCase() === name.toLowerCase()
+    });
+  });
+
 
 function fuzzyMatch(drivers, letters) {
   return drivers.filter (function (driver){
