926c1f0fdd0b96dd808899be43c847365ee539a4
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-04-09 21:51:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 // Code your solution in this file
+function findMatching (drivers, name) {
+  return drivers.filter(function (driver) {return driver.toLowerCase() === name.toLowerCase();});
+}
 
-function findMatching(drivers, name) {
- return drivers.filter(function (driverName) {
- return driverName.toLowerCase() === name.toLowerCase()
-    });
-  });
+function fuzzyMatch (drivers, letters) {
+  return drivers.filter(function (driver) {return driver.slice(0, letters.length) === letters;});
+}
 
-function fuzzyMatch(drivers, partialName) {
-  let lengthOfName = partialName.length;
-  return drivers.filter(function (driverName) {
-    return driverName.slice(0, lengthOfName) === partialName;
-  });
-};
-
-function matchName(drivers, name) {
-return drivers.filter (function (driver) {
- return driver.name.toLowerCase() === name.toLowerCase();
-    });
-};
+function matchName (drivers, string) {
+  return drivers.filter(function (driver) {return driver.name === string;});
+}
